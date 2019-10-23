d376ef5330a00377848227f361095efef3cc2de7
ruhara chadrack <alfredruhara@gmail.com>
2019-10-23 19:42:21 +0200

    chore(codebase) : config dependencies - add dependcies confi files - initailize the template codebase [start #169321986]

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  const error = new Error('NOT FOUND');
+  error.status = 403;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+export default app;
